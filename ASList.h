f5c12d6d8c918a1b9e59972ba9abe9598ff6f2fa
Andrew Merritt <a0merr05@louisville.edu>
2025-10-30 07:25:35 -0400

    ASList.h

--- ASList.h
+++ ASList.h
@@ -8,42 +8,19 @@ template <class T>
 class ASList {
 private:
     static const int MAX_SIZE = 1000;  
-    T elements[MAX_SIZE];
-    int length;
-
+    T elements[MAX_SIZE];              
+    int length;                       
 public:
-    ASList() : length(0) {}
+    ASList();                          
 
-    bool isFull() const {
-        return length == MAX_SIZE;
-    }
+    bool isFull() const;               
+    bool isEmpty() const;              
 
-    bool isEmpty() const {
-        return length == 0;
-    }
-
-    void insert(const T& item) {
-        if (isFull()) {
-            cout << "List is full, cannot insert more items.\n";
-            return;
-        }
-
-        int i = length - 1;
-        while (i >= 0 && item < elements[i]) {
-            elements[i + 1] = elements[i];
-            i--;
-        }
-        elements[i + 1] = item;
-        length++;
-    }
-
-    void print() const {
-        for (int i = 0; i < length; i++) {
-            cout << elements[i].country << ", "
-                 << elements[i].literacyRate << ", "
-                 << elements[i].gdpPerCapita << endl;
-        }
-    }
+    void insert(const T& item);        
+    void print() const;                
 };
 
+#include "ASList.cpp"
+
 #endif
+
